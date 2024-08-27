dc2ff1202f6ea9c010def324b4812742b4fa9e62
Vincent Bowen <vibo3564@colorado.edu>
2024-08-26 19:11:50 -0600

    fixing navbar grid spacing

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -51,7 +51,7 @@ export default function Navbar() {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
+            <div className="flex flex-shrink-0 items-center -ml-12">
                 <HomeLogo/>
             </div>
           </div>
