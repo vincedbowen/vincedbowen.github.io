817e924ce86b5aa5c38cf172ef675df522289a08
Vincent Bowen <vibo3564@colorado.edu>
2025-06-30 20:49:34 -0600

    forgot favicon

--- app/layout.js
+++ app/layout.js
@@ -7,11 +7,15 @@ const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 export const metadata = {
   title: "Vincent Bowen",
   description: "Vincent Bowen's Personal website",
+  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/vdbHome.png" />
+      </head>
       <body className={poppins.className}>
         <AppShell>{children}</AppShell>
       </body>
