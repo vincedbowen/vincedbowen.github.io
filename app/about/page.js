780779426390c37720ca77df7d805164a426d7df
Vincent Bowen <vibo3564@colorado.edu>
2025-06-30 20:32:26 -0600

    fixing lint

--- app/about/page.js
+++ app/about/page.js
@@ -8,10 +8,10 @@ export default function About() {
       <div className="max-w-3xl">
         <p className="text-lg text-right">
           Hello {`ツ` } I am a recent graduate from the University of Colorado Boulder with a Bachelor of Science in Computer Science.
-          This summer I am continuing my education here in Boulder where I am beginning my Master's degree in Biomedical Engineering.
+          This summer I am continuing my education here in Boulder where I am beginning my Master&apos;s degree in Biomedical Engineering.
           <br /><br />
           I am extremely passionate about using technology and software to solve challenges in our healthcare and medical system.
-          I'm an intern at The Cigna Group, where I work with big data and am currently learning Rust 🦀. 
+          I&apos;m an intern at The Cigna Group, where I work with big data and am currently learning Rust 🦀. 
           <br /><br />
           During my free time, I enjoy cycling, my girlfriend and I love to train our dog Gary, and I listen to a lot of music.
         </p>
